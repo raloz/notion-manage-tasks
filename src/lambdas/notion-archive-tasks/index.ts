7ae36cda46f9b1e62a4bc53fedb5c48aced42c74
Octavio Quiroz <octavio.lopez@kavak.com>
2023-03-15 01:08:08 -0600

    feat: se instala dotenv en packaje.json y cambios menores en el index.js

--- src/lambdas/notion-archive-tasks/index.ts
+++ src/lambdas/notion-archive-tasks/index.ts
@@ -1,11 +1,9 @@
 import { getTasks, archiveTask } from "../../modules/notion-client";
 import { NotionIdentificator } from "../../types/notion";
- 
+
 export const archiveTasksNotion = () => {
     getTasks()
         .then(async (tasks) => {
-            console.log(JSON.stringify(tasks));
-
             const { results } = tasks;
             const archiveTasksPromise = results.map(task => archiveTask(task.id as NotionIdentificator));
 
